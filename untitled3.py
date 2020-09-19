a5f5a6e5fc3de1ce8aad565957dabe61bd589d6a
jerry1118800 <71535234+jerry1118800@users.noreply.github.com>
2020-09-19 09:39:51 +0800

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 11:25:48 2020
+
+@author: ASUSER
+"""
+
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+print (mc.player.getTilePos())
